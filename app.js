c9ae2660c176445fd10d62dd435b46a0b37ba5c0
Алексей Кузнецов <alexeykuznetsov@MacBook-Air-Aleksej.local>
2022-07-27 22:03:17 +0300

    create project

---
+++ app.js
@@ -0,0 +1,31 @@
+const board = document.querySelector('#board')
+const colors = ['#f54ee4', '#f54ea7', '#f72f5e',
+    '#f75e2f', '#f4f72f', '#86f72f', '#2ff7d3']
+const SQUARES_NUMBER = 600
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px $(color), 0 0 10px $(color)`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
